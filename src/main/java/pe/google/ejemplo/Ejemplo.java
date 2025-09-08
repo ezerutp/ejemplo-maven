4cd73e5f633a8c7a2a9941fb26fb5d5b8688ed54
ezerutp <u22320821@utp.edu.pe>
2025-09-08 01:29:51 -0500

    fix: se corrigio el error de sintaxis  - Dia 2 - 18/08

--- src/main/java/pe/google/ejemplo/Ejemplo.java
+++ src/main/java/pe/google/ejemplo/Ejemplo.java
@@ -3,6 +3,5 @@ package pe.google.ejemplo;
 public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo simple sin cambios jajaja");
     }
 }
